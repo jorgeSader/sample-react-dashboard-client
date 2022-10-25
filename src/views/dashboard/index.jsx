72ca7f0b8f0ab9f83a3cbea2108dc29d3a9322af
Jorge Sader <Jorge.Sader@gmail.com>
2022-10-25 18:10:53 -0400

    add bar and geography charts to dashboard

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -227,6 +227,63 @@ export default function Dashboard() {
             </Box>
           ))}
         </Box>
+
+        {/* ROW 3 */}
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          p="30px"
+        >
+          <Typography variant="h5" fontWeight={'600'}>
+            Campaign
+          </Typography>
+          <Box
+            display={'flex'}
+            flexDirection="column"
+            alignItems={'center'}
+            mt="25px"
+          >
+            <ProgressCircle size="125" />
+            <Typography variant="h5" color={colors.greenAccent[500]} mt="15px">
+              129,438.78 Revenue Generated
+            </Typography>
+            <Typography> Includes extra Misc expenditures and costs</Typography>
+          </Box>
+        </Box>
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Typography
+            variant="h5"
+            fontWeight={'600'}
+            sx={{ p: '30px 30px 0 30px' }}
+          >
+            Sales Quantity
+          </Typography>
+          <Box height="250px" mt="-20px">
+            <BarChart isDashboard />
+          </Box>
+        </Box>
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          padding="30px"
+        >
+          <Typography variant="h5" fontWeight={'600'} sx={{ mb: '15px' }}>
+            Geography Based Traffic
+          </Typography>
+          <Box height="200px">
+            <GeographyChart isDashboard />
+          </Box>
+        </Box>
+
+        {/*  */}
       </Box>
     </Box>
   );
