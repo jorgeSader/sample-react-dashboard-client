b086c76eff505d545c900ca2e9c4eb721eff9f16
Jorge Sader <Jorge.Sader@gmail.com>
2022-11-03 10:49:11 -0400

    removed unused package

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,5 @@
 import { createContext, useState, useMemo } from 'react';
 import { createTheme } from '@mui/material/styles';
-// import { dark } from '@mui/material/styles/createPalette';
 
 // color design tokens
 export const tokens = (mode) => ({
